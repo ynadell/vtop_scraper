cbb5e5a37764a85416b03b45c11cef59f57eed05
yashwanth-nadella <yashwanthnadella@gmail.com>
2022-07-18 10:00:30 +0530

    Basic code

--- scarping_medium_blog.py
+++ scarping_medium_blog.py
@@ -1,6 +1,9 @@
-import bs4
+from bs4 import BeautifulSoup
 import requests
 import re
 
 url = "https://medium.com/@yashwanthnadella/why-care-about-climate-change-390906b8b4bd"
+result = requests.get(url).text
+doc = BeautifulSoup(result,"html.parser")
 
+print(doc.prettify())
